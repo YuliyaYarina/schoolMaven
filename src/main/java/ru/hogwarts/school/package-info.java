2e6a6261ca4b90dbdae70807b2f5c2466886a068
ADMIN <Yulechka140200@gmail.com>
2024-02-29 22:48:29 +0500

    ?

---
+++ src/main/java/ru/hogwarts/school/package-info.java
@@ -0,0 +1,14 @@
+package ru.hogwarts.school;
+
+//"для наглядности"
+
+//
+//select * from student where age > 9 SemanticContext.AND age<20;
+//
+//select name from student ;
+//
+//select * from student where name like '%D%';
+//
+//select * from student where age < id;
+//
+//select * from student ORDER BY age;
